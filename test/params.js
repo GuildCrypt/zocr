792fd5d0fd98a490ffff8dea883bbb90896ca975
Aakil Fernandes <aakilfernandes@gmail.com>
2019-03-06 19:18:35 -0500

    version byte

--- test/params.js
+++ test/params.js
@@ -11,6 +11,7 @@ module.exports = {
   nullAddress: new Amorph((new Uint8Array(20)).fill(0)),
   exchangeAddress: Amorph.from(amorphHex.prefixed, '0x4f833a24e1f95d70f028921e27040ca56e09ab0b'),
   order0: {
+    version: new Amorph(new Uint8Array([0])),
     makerAssetAddress: getRandomAmorph(20),
     takerAssetAddress: getRandomAmorph(20),
     makerAssetAmount: Amorph.from(amorphNumber.unsigned, 100),
@@ -19,6 +20,7 @@ module.exports = {
     salt: getRandomAmorph(6),
   },
   order1: {
+    version: new Amorph(new Uint8Array([0])),
     makerAssetAddress: getRandomAmorph(20),
     takerAssetAddress: getRandomAmorph(20),
     makerAssetAmount: Amorph.from(amorphNumber.unsigned, 200),
